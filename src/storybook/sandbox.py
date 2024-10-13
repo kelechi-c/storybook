47b6ac9f5d73cd326ad21529418b7e7ef4cb8fbf
Tensor <narutokelechi@gmail.com>
2024-10-13 04:55:17 +0100

    [modify] add sandbox file as pastebin

--- src/storybook/sandbox.py
+++ src/storybook/sandbox.py
@@ -1,28 +1,28 @@
-import torch
 from diffusers import (
     StableCascadeDecoderPipeline,
     StableCascadePriorPipeline,
     StableCascadeUNet,
 )
+# model ids
+cascade_model_id = "stabilityai/stable-cascade"
+cascade_prior_id = "stabilityai/stable-cascade-prior"
+# testing text prompt
+prompt = "dim-lit painting of two canary birds renaissance style"
+negative_prompt = "low quality"
 
-prompt = "an image of a shiba inu, donning a spacesuit and helmet"
-negative_prompt = ""
-
+# unet models for stage C and B
 prior_unet = StableCascadeUNet.from_pretrained(
     "stabilityai/stable-cascade-prior", subfolder="prior_lite"
 )
+
 decoder_unet = StableCascadeUNet.from_pretrained(
     "stabilityai/stable-cascade", subfolder="decoder_lite"
 )
 
-prior = StableCascadePriorPipeline.from_pretrained(
-    "stabilityai/stable-cascade-prior", prior=prior_unet
-)
-decoder = StableCascadeDecoderPipeline.from_pretrained(
-    "stabilityai/stable-cascade", decoder=decoder_unet
-)
+# stage C model for highly compressed latent generation
+prior = StableCascadePriorPipeline.from_pretrained(cascade_prior_id, prior=prior_unet) # load prior for stage C
 
-prior.enable_model_cpu_offload()
+prior.enable_model_cpu_offload() # to save memory during inference
 prior_output = prior(
     prompt=prompt,
     height=1024,
@@ -33,6 +33,12 @@ prior_output = prior(
     num_inference_steps=20,
 )
 
+
+# load upscaler/decoder for stage B/A
+decoder = StableCascadeDecoderPipeline.from_pretrained(
+    cascade_model_id, decoder=decoder_unet
+) 
+
 decoder.enable_model_cpu_offload()
 decoder_output = decoder(
     image_embeddings=prior_output.image_embeddings,
@@ -42,4 +48,5 @@ decoder_output = decoder(
     output_type="pil",
     num_inference_steps=10,
 ).images[0]
-decoder_output.save("cascade.png")
+
+decoder_output.save("image_cascade.png")
